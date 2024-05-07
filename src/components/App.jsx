42f5510ffa00c8d48cde173457cb2ffb964310fc
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2024-05-07 17:56:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
+const dfe = {
+  _id: "1",
+  name: "Road Bear C 23-25",
+  price: 10000,
+ avatar: "https://ftp.goit.study/img/campers-test-task/1-1.webp",
+}
+
+
+
 export const App = () => {
   return (
     <div>
     <h1>Campers-rent</h1>
+<img src={dfe.avatar} alt={dfe.name} width="200" />
     </div>
   );
 };
